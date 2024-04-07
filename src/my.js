165ab0593dc1cedf7d7ef88844623dad8d27b75d
sai <jayasaivardhan.jv@gmail.com>
2024-04-07 20:12:37 +0530

    init

---
+++ src/my.js
@@ -0,0 +1,16 @@
+function Birthday() {
+
+  
+  
+  return (
+    <div> 
+      <div id='birthday-line'>7 wonders in the world and it's your 7th Birthday!</div>
+      <div id='footer-div'>
+      <footer id='footer'>Made with &#10084;</footer>
+      </div>
+      
+      </div>
+  );
+}
+
+export default Birthday;
